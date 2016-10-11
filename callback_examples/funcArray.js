1acc4aae57ffd975280e5d909d6b3b4028bf0b17
Stanislaw Alam <stan.alam@gmail.com>
2016-10-11 09:28:55 -0400

    update functional array imperative vs declarative

---
+++ callback_examples/funcArray.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < array.length; i++) {
+	array[i] = Math.pow(array[i], 2);
+}
+array;
---
+++ callback_examples/funcArray2.js
@@ -0,0 +1,5 @@
+'use strict';
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(
+	function(num) {
+		return Math.pow(num, 2);
+	});
