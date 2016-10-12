a5d6522b2278fb8cd35ff336638cdc609e3f5a20
Stanislaw Alam <stan.alam@gmail.com>
2016-10-11 22:20:34 -0400

    updated sort array

---
+++ callback_examples/sortArray.js
@@ -0,0 +1,31 @@
+//sort array elements with callbacks
+'use strict';
+
+var myArr = [{
+
+    num: 5,
+    str: 'apple'
+
+}, {
+
+    num: 7,
+    str: 'cabbage'
+
+}, {
+
+    num: 1,
+    str: 'orange'
+
+}];
+
+myArr.sort(function(val1, val2) {
+    if (val1.str  > val2.str) {
+        return -1;
+    } else {
+        return 1;
+    }
+
+});
+
+
+console.log(myArr);
