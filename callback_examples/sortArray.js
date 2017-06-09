b458ee236dcf37925caaca2b4127af3ea454aa0f
Stanislaw Alam <stan.alam@gmail.com>
2017-06-08 21:05:26 -0400

    javascript beautify

--- callback_examples/sortArray.js
+++ callback_examples/sortArray.js
@@ -3,23 +3,26 @@
 
 var myArr = [{
 
-    num: 5,
-    str: 'apple'
+        num: 5,
+        str: 'apple'
 
-}, {
+    },
+    {
 
-    num: 7,
-    str: 'cabbage'
+        num: 7,
+        str: 'cabbage'
 
-}, {
+    },
+    {
 
-    num: 1,
-    str: 'orange'
+        num: 1,
+        str: 'orange'
 
-}];
+    }
+];
 
 myArr.sort(function(val1, val2) {
-    if (val1.str  > val2.str) {
+    if (val1.str > val2.str) {
         return -1;
     } else {
         return 1;
