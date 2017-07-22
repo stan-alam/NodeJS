ba1f6980a7242772169058425136756e460c0938
stan <stan.alam@gmail.com>
2017-07-22 02:01:43 +0100

    updated notes, also updated app2.js to use node 8, which is the latest stable as of this writing

---
+++ completeC2/2/nodeNotes/app2.js
@@ -0,0 +1,11 @@
+console.log('use node version > 4 ');
+console.log('start app2');
+
+const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+console.log(user);
+
+fs.appendFile('username.txt', 'Hello world!' + user.username + '!');
+//fs.appendFile
