93bf915def1989e42e34455db1dc7840e4fc67b6
Stan Alam <stan.alam@gmail.com>
2018-03-02 16:19:45 -0500

    completed count even function to count even numbers in an ascii file. Using async and await to run async code in a linear - waterfall step(s)

--- coreNode/10/15-30/src/countEven.js
+++ coreNode/10/15-30/src/countEven.js
@@ -1,3 +1,14 @@
 async function countEven() {
-
+//  (%) is the modulus operator, it will let you have the remainder of place/sequence.length. taken from stackoverflow
+try {
+  const lines = await readFileAsArray('./numbers');
+  const numbers = lines.map(Number);
+  const evenCount = numbers.filter(number => number % 2 !== 1).length
+  console.log('even number count is : ', evenCount);
+} catch(err) {
+  console.error(err);
 }
+  }
+}
+
+countEven(); // wow! async code running synchronously ... good for test automation!
