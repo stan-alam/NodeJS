2f8a45e405250b23664271836ce9df180a1b5681
Stan Alam <stan.alam@gmail.com>
2018-03-04 05:53:28 -0500

    completed async_event.js, shows time elapsed on an exec of async function

--- coreNode/10/15-30/src/async_event.js
+++ coreNode/10/15-30/src/async_event.js
@@ -6,7 +6,20 @@ execute(asyncFunc, ...args) {
   console.time('execute');
   this.emit('begin');
   asyncFunc(...args, (err, data) => {
+    if(err) {
+      return this.emit('error', err);
+    }
 
-  })
+    this.emit('data', data);
+    console.timeEnd('execute');
+    this.emit('end');
+  });
+  }
 }
-}
+
+const elapsedTime = new ElapsedTime();
+
+elapsedTime.on('begin', () => console.log('ready to exec'));
+elapsedTime.on('end', () => console.log('completed execution'));
+
+elapsedTime.execute(fs.readFile, __filename);
