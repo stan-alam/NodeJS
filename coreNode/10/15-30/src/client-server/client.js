438f1045aff3f70655a42bc5ec544b0fc8ebe626
Stan Alam <stan.alam@gmail.com>
2018-03-06 09:42:45 -0500

    added interface for eventEmitter in client

--- coreNode/10/15-30/src/client-server/client.js
+++ coreNode/10/15-30/src/client-server/client.js
@@ -1,1 +1,9 @@
 const EventEmitter = require('events');
+const readLine = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdn, output:process.stdout
+});
+
+//client event emmiter
+const client = new EventEmitter();
