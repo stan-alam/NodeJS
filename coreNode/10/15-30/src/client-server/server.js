1bf8393dadd3c7b52b80295f79ef4f56beb1290b
Stan Alam <stan.alam@gmail.com>
2018-03-18 12:56:35 -0400

    updated SERVER to listen to command events coming from client, the console.log will echo the command, to demonstrate the command echo

--- coreNode/10/15-30/src/client-server/server.js
+++ coreNode/10/15-30/src/client-server/server.js
@@ -3,7 +3,23 @@ const EventEmitter = require('events');
 class Server extends EventEmitter {
   constructor(client) { //define the constructor to receive the client *within the server class
     super();
+    client.on('command', (command) => {
+      console.log(`Command: ${command}`);
+      // help, add, del, ls
+    });
   }
+    help() {
+      this.emit('response', 'help is on the way ...');
+    }
+    add() {
+      this.emit('response', 'adding ...');
+      }
+   del() {
+     this.emit('response', 'deleting ...');
+   }
+   ls(){
+     this.emit('response', 'ls-Ing...');
+   }
 }
 //created a function not just an object
 module.exports = (client) => new Server(client); // Server(client) instantiate the server object with the client object
