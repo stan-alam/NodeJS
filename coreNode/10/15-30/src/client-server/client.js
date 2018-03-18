3ec124ad546d797ffe36c794bfc0b508ba570ab6
Stan Alam <stan.alam@gmail.com>
2018-03-18 13:21:42 -0400

    updated client with a function to listen to the server response via a function(resp)

--- coreNode/10/15-30/src/client-server/client.js
+++ coreNode/10/15-30/src/client-server/client.js
@@ -12,6 +12,9 @@ const server = require('./server')(client); //import server object
 /* the client is going to emit events, while
 the server is going to listen to those events
 */
+server.on('response', (resp) => { //when the server emits a response event, the listener will access the response as a function
+  console.log(`Response: ${resp}`);
+});
 //use the readline interInterface
 rl.on('line', (input) => { //here register a listener for the line event, that receives an input
   client.emit('command', input);  // now everytime the user presses ENTER,
