61fdde01fb5365debadc3ea7e0db137fa049910c
Stan Alam <stan.alam@gmail.com>
2018-04-04 15:38:30 -0400

    created another readstream that will print characters up to z, and stream these characters to stdout, with a 100 millasecond interval between each character.

---
+++ coreNode/10/40-50/src/readStream2.js
@@ -0,0 +1,26 @@
+const { Readable } = require('stream'); // implement a readable stream, require a READABLE interface, and construct an object
+
+// const streamIn = new Readable();
+//
+// streamIn.push('abc, 123');
+// streamIn.push(null);
+// we're pushing all data into the stream before piping it to stdout
+// streamIn.pipe(process.stdout);
+
+
+// it is better to push data when consumer demands it.
+
+const streamIn = new Readable({
+  read(size) {
+    setTimeout(() => {
+      this.push(String.fromCharCode(this.currentCharCode++));
+      if(this.currentCharCode > 90){ //stop at letter z
+        this.push(null);
+    }
+  }, 100);
+  }
+});
+
+streamIn.currentCharCode = 65;
+
+streamIn.pipe(process.stdout);
