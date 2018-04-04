83826d1f7c7d54ec0284ad675534ade5238bf882
Stan Alam <stan.alam@gmail.com>
2018-04-04 15:33:05 -0400

    added logic to streamIn so that consumer can request data in chunks to be piped into stdout

--- coreNode/10/40-50/src/readStream.js
+++ coreNode/10/40-50/src/readStream.js
@@ -1,8 +1,25 @@
 const { Readable } = require('stream'); // implement a readable stream, require a READABLE interface, and construct an object
 
-const inStream = new Readable();
+// const streamIn = new Readable();
+//
+// streamIn.push('abc, 123');
+// streamIn.push(null);
+// we're pushing all data into the stream before piping it to stdout
+// streamIn.pipe(process.stdout);
 
-inStream.push('abc, 123');
-inStream.push(null);
 
-inStream.pipe(process.stdout);
+// it is better to push data when consumer demands it.
+
+const streamIn = new Readable({
+  read(size) {
+    setTimeout(1000);
+    this.push(String.fromCharCode(this.currentCharCode++));
+    if(this.currentCharCode > 90){
+      this.push(null);
+    }
+  }
+});
+
+streamIn.currentCharCode = 65;
+
+streamIn.pipe(process.stdout);
