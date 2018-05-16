5ba9c61ca2cbe2981e99dee0c383dd1bd44375ca
Stan Alam <stan.alam@gmail.com>
2018-05-16 18:57:23 -0400

    updated course work for NodeJS

---
+++ coreNode/10/40-50/src/zeroDwnTime/MstrCluster.js
@@ -0,0 +1,40 @@
+//MasterCluster.js
+const cluster = require('cluster');
+const os = require('os');
+
+if (cluster.isMaster){
+  const cpus = os.cpus().length;
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork();
+   }
+
+   cluster.on('exit', (worker, code, signal) => {
+     if (code !== 0 && !worker.exitedAfterDisconnect) {
+       console.log(`Worker ${worker.id} has crashed. ` +
+         'Spinning up a new worker...');
+      cluster.fork();
+     }
+  });
+
+process.on('SIGUSR2', () => {
+  const workers = Object.values(cluster.workers);
+
+  const restartWorker = (workerIndex) => {
+    const worker = workers[workerIndex];
+    if (!worker)
+      return;
+
+    worker.on('exit', () => {
+      if (!worker.exitedAfterDisconnect) //set flag if true, fork new worker
+      console.log();
+      return;
+    });
+      worker.disconnect();
+  };
+
+  restartWorker(0);
+});
+
+} else {
+  require('./HttpServer');
+}
