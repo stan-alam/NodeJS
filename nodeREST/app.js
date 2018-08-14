296dffa8753c3e1bc8744da582779c6e87f00f06
Stan Alam <stan.alam@gmail.com>
2018-08-13 23:58:09 -0400

    removed stale code

--- nodeREST/app.js
+++ nodeREST/app.js
@@ -4,28 +4,11 @@ const fs = require('fs'),
     fsExtra = require('fs-extra'),
     express = require('express'),
     app = express(),
-    sleep = require('system-sleep'),
     spawn = require('child_process').exec,
     shell = require('shelljs'),
-    path = require('path'),
-    fileExists = require('file-exists');
-
-//Setup Selenium
-var logdir = new Date().getTime();
-
-try{
-    fs.mkdirSync("logs");
-    fs.mkdirSync("screenshots");
-} catch(e){
-    console.info("logs already created");
-}
-fs.mkdirSync("logs/"+logdir);
+    path = require('path')
 
 app.get('/', (req, res) => res.spawn('MyFile.bat', function(error, stdout, stderr) {
     console.log(stdout);
   })
 );
-
-
-
-
