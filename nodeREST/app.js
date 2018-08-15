0d7c373b75461c7779869f2c644e7b3750ed62d7
Stan Alam <stan.alam@gmail.com>
2018-08-14 21:19:30 -0400

    updated code, node server will execute a windows batch when a get request is sent to localhost:8888

--- nodeREST/app.js
+++ nodeREST/app.js
@@ -4,14 +4,16 @@ const fs = require('fs'),
     fsExtra = require('fs-extra'),
     express = require('express'),
     app = express(),
-    spawn = require('child_process').exec,
-    shell = require('shelljs'),
-    path = require('path')
+    spawn = require('child_process').exec
 
 app.get('/', (req, res) => spawn('MyFile.bat', function(error, stdout, stderr) {
     console.log(stdout);
-//this will excute the MyFile.bat, that will run the test and send response
-//to CI server -- 
+/*this will excute the MyFile.bat, that will run the test and send response pass/fail status
+to a CI server -- incase you're running an isolated network, and don't wish to poison
+the in house network
+*/
    process.exit(0);
   })
 );
+
+app.listen(8888, () => console.log('listening on port 8888'))
